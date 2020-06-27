90ed39b432c7fbc4d71652c0dad1781b50faef7a
bilwa93 <bilwa.p1893@gmail.com>
2020-06-27 14:35:08 +0530

    new

---
+++ BasicCoreJava/src/BasicCoreJava/BasicJavaProgram.java
@@ -0,0 +1,12 @@
+package BasicCoreJava;
+
+public class BasicJavaProgram {
+
+	public static void main(String[] args) {
+		
+		System.out.println("New Program!");
+		// TODO Auto-generated method stub
+
+	}
+
+}
